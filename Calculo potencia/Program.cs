30b1a2a2df9b51a406f33f5e528fb2e7dbeb2c34
Joao Victor Baptista <joaovictordacostabaptista@hotmail.com>
2024-04-16 02:34:37 -0300

    Add project files.

---
+++ Calculo potencia/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Digite um número: ");
+        string input = Console.ReadLine();
+        float numero1;
+        bool sucesso = float.TryParse(input, out numero1);
+
+        Console.WriteLine("Digite o expoente: ");
+        string input2 = Console.ReadLine();
+        int potencia;
+        bool sucesso2 = int.TryParse(input2, out potencia);
+
+        if (!sucesso)
+        {
+            Console.WriteLine("Entrada inválida. Por favor, digite um número válido.");
+            return;
+        }
+
+        if (!sucesso2 || potencia < 0)
+        {
+            Console.WriteLine("Potência inválida. Por favor, digite um número inteiro não negativo.");
+            return;
+        }
+
+        float resultado = 1;
+        for (int i = 0; i < potencia; i++)
+        {
+            resultado *= numero1;
+        }
+
+        Console.WriteLine($"O resultado de {numero1} elevado a {potencia} é: {resultado}");
+    }
+
+}
+
+    
+
